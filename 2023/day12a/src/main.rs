5c10e171f0153210a7c5832ec06865f32719b8bf
Fabian Weik <fabian.weik@protonmail.com>
2023-12-12 20:44:34 +0100

    day 12 a: start rewrite

--- 2023/day12a/src/main.rs
+++ 2023/day12a/src/main.rs
@@ -7,6 +7,8 @@ use std::{fs::File, io::Read, str::FromStr, usize};
 use anyhow::{anyhow, Error};
 use clap::Parser;
 
+use iter_tools::Itertools;
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -30,94 +32,25 @@ fn main() {
 fn run(content: &str) -> usize {
     let records = parse(content);
 
-    records.iter().map(Record::process).sum()
+    for r in records {
+        println!("{:?}", r)
+    }
+
+    //    records.iter().map(Record::process).sum()
+    0
+}
+
+#[derive(Debug)]
+enum ConditionGroup {
+    Operational,
+    Damaged(usize),
+    Unknown(usize),
 }
 
 #[derive(Debug)]
 struct Record {
-    damaged: Vec<Option<bool>>,
+    conditions: Vec<ConditionGroup>,
     groups: Vec<usize>,
-}
-
-impl Record {
-    fn process(&self) -> usize {
-        process_rec(&self.damaged, &self.groups)
-    }
-}
-
-fn process_rec(damaged: &Vec<Option<bool>>, groups: &Vec<usize>) -> usize {
-    let complete = damaged.iter().all(|d| *d != None);
-
-    if !grouping_possible(damaged, groups, complete) {
-        return 0;
-    }
-
-    if complete {
-        return 1;
-    }
-
-    let (next_false, next_true) = create_reucursion_damaged(damaged);
-    process_rec(&next_false, groups) + process_rec(&next_true, groups)
-}
-
-fn grouping_possible(damaged: &Vec<Option<bool>>, groups: &Vec<usize>, complete: bool) -> bool {
-    let mut curr_groups = vec![];
-    let mut curr_group = 0;
-
-    let mut last_incomplete = damaged.len() == 0;
-    for d in damaged {
-        match d {
-            None => {
-                if curr_group > 0 {
-                    curr_groups.push(curr_group);
-                    curr_group = 0;
-                    last_incomplete = true;
-                }
-                break;
-            }
-            Some(true) => curr_group += 1,
-            Some(false) => {
-                if curr_group > 0 {
-                    curr_groups.push(curr_group);
-                    curr_group = 0;
-                }
-            }
-        }
-    }
-    if curr_group > 0 {
-        curr_groups.push(curr_group);
-    }
-
-    let mut pairs = curr_groups.iter().zip(groups.iter()).peekable();
-    let mut res = !complete || curr_groups.len() == groups.len();
-    while let Some((c, g)) = pairs.next() {
-        res = res
-            && if last_incomplete && pairs.peek() == None {
-                c <= g
-            } else {
-                c == g
-            }
-    }
-
-    res
-}
-
-fn create_reucursion_damaged(
-    damaged: &Vec<Option<bool>>,
-) -> (Vec<Option<bool>>, Vec<Option<bool>>) {
-    let (index, _) = damaged
-        .iter()
-        .enumerate()
-        .find(|(_, d)| *d == &None)
-        .expect("We know that one element must be none");
-
-    let mut next_false = damaged.clone();
-    next_false[index] = Some(false);
-
-    let mut next_true = damaged.clone();
-    next_true[index] = Some(true);
-
-    (next_false, next_true)
 }
 
 // Parsing
@@ -129,20 +62,23 @@ fn parse(content: &str) -> Vec<Record> {
         .collect::<Result<_, _>>()
         .expect("someting went wrong during parsing")
 }
+
 impl FromStr for Record {
     type Err = Error;
 
     fn from_str(line: &str) -> Result<Self, Self::Err> {
         let mut parts = line.split_ascii_whitespace();
 
-        let damage_part = parts.next().expect("we know it has two parts");
-        let damaged = damage_part
+        let condition_part = parts.next().expect("we know it has two parts");
+        let conditions = condition_part
             .chars()
-            .map(|c| match c {
-                '.' => Ok(Some(false)),
-                '#' => Ok(Some(true)),
-                '?' => Ok(None),
-                _ => Err(anyhow!("unknown character in input: {}", c)),
+            .group_by(|c| c.clone())
+            .into_iter()
+            .map(|(c, group)| match c {
+                '.' => Ok(ConditionGroup::Operational),
+                '#' => Ok(ConditionGroup::Damaged(group.count())),
+                '?' => Ok(ConditionGroup::Unknown(group.count())),
+                _ => Err(anyhow!("unexpected character in input: {}", c)),
             })
             .collect::<Result<_, _>>()?;
 
@@ -152,7 +88,7 @@ impl FromStr for Record {
             .map(usize::from_str)
             .collect::<Result<_, _>>()?;
 
-        Ok(Record { damaged, groups })
+        Ok(Record { conditions, groups })
     }
 }
 
@@ -174,6 +110,7 @@ mod tests {
         assert_eq!(result, 21)
     }
 
+    /*
     #[test]
     fn test_long() {
         let file = "long_data";
@@ -194,4 +131,5 @@ mod tests {
 
         b.iter(|| run(&content));
     }
+    */
 }
