bfe8a619aba403a7c4c3e22772951e488dad19d4
Fabian Weik <fabian.weik@protonmail.com>
2023-12-15 11:05:36 +0100

    day 15 a : start

---
+++ 2023/day15a/src/main.rs
@@ -0,0 +1,92 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::{
+    collections::HashSet,
+    fmt::{Display, Write},
+    fs::File,
+    io::Read,
+};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    file: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let file = args.file;
+
+    let mut file = File::open(file).unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+
+    let result = run(&content);
+
+    println!("{}", result)
+}
+
+fn run(content: &str) -> usize {
+    let parts = parse(content);
+
+    parts.map(process)
+}
+
+fn process(part: &str) -> usize {
+    0
+}
+
+// Parsing
+
+fn parse(content: &str) -> impl Iterator<Item = &str> {
+    content.split(",")
+}
+
+// testing
+#[cfg(test)]
+mod tests {
+    use core::panic;
+
+    use ::test::Bencher;
+
+    use super::*;
+
+    #[test]
+    fn test_short() {
+        let file = "short_data";
+        let mut file = File::open(file).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+
+        let result = run(&content);
+        assert_eq!(result, 1320)
+    }
+
+    /*
+    #[test]
+    fn test_long() {
+        let file = "long_data";
+        let mut file = File::open(file).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+
+        let result = run(&content);
+        assert_eq!(result, 104409)
+    }
+
+    #[bench]
+    fn bench(b: &mut Bencher) {
+        let file = "long_data";
+        let mut file = File::open(file).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+
+        b.iter(|| run(&content));
+    }
+    */
+}
