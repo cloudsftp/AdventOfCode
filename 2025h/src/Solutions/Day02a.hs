fbbafa382b0dab79ce2a2d16a8dbd8a6c80c7c90
Fabian Weik <fabian.weik@protonmail.com>
2025-12-02 20:10:55 +0100

    parse range inputs into range parts

--- 2025h/src/Solutions/Day02a.hs
+++ 2025h/src/Solutions/Day02a.hs
@@ -2,5 +2,38 @@ module Solutions.Day02a
   ( solve
   ) where
 
+import Lib
+
 solve :: String -> Int
-solve _ = 0
+solve input =
+  let ranges = parseRanges input
+  in sum $ map end ranges 
+
+data Range = Range { start :: Int
+                   , end :: Int
+                   } deriving (Show)
+
+-- input example : 1-200,2-5
+parseRanges :: String -> [Range]
+parseRanges = parseRangesRec []
+
+parseRangesRec :: [Range] -> String -> [Range]
+parseRangesRec acc [] = acc
+parseRangesRec acc input =
+  let (rangeInput, rest) = debug $ splitString ',' input
+      range = parseRange rangeInput
+  in parseRangesRec (range:acc) rest
+
+-- input example: 1-200
+parseRange :: String -> Range
+parseRange input =
+  let (startInput, endInput) = splitString '-' input
+      start = read startInput
+      end = read endInput
+  in Range { start = start, end = end }
+
+splitString :: Char -> String -> (String, String)
+splitString c input =
+  let part1 = takeWhile (/= c) input
+      part2 = dropWhile (== c) $ dropWhile (/= c) input
+  in (part1, part2)
