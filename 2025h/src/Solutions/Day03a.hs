78a3d41fb9fe8241dd2c0ee245810319f86bd55a
Fabian Weik <fabian.weik@protonmail.com>
2025-12-03 18:52:43 +0100

    implement first attempt of logic

--- 2025h/src/Solutions/Day03a.hs
+++ 2025h/src/Solutions/Day03a.hs
@@ -4,15 +4,36 @@ module Solutions.Day03a
 
 import Debug.Trace
 
+type Bank = [Int]
+
 solve :: String -> Int
-solve input = trace ("parsed input: " ++ show (parseInput input)) 0
+solve input = 
+  let banks = parseInput input
+  in sum $ map bankJoltage banks
+
+bankJoltage :: Bank -> Int
+bankJoltage [_] = 0
+bankJoltage bank = max (bankJoltageOuter bank)
+                       (bankJoltageOuter $ tail bank)
+
+bankJoltageOuter :: Bank -> Int
+bankJoltageOuter bank =
+  let (first:rest) = bank
+  in bankJoltageInner first rest
+
+
+bankJoltageInner :: Int -> Bank -> Int
+bankJoltageInner _ [] = 0
+bankJoltageInner first (d:ds) = max (10 * first + d)
+                                    (bankJoltageInner first ds)
+
 
 -- parsing
 
-parseInput :: String -> [[Int]]
+parseInput :: String -> [Bank]
 parseInput input = map parseLine $ lines input
 
-parseLine :: String -> [Int]
+parseLine :: String -> Bank 
 parseLine = map parseCharacter
 
 parseCharacter :: Char -> Int
