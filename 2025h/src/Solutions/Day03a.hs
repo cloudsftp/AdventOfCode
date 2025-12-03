4a57216ce2fc5d5296a34e78d7dea5b8328fc116
Fabian Weik <fabian.weik@protonmail.com>
2025-12-03 18:33:15 +0100

    parsing part 1

--- 2025h/src/Solutions/Day03a.hs
+++ 2025h/src/Solutions/Day03a.hs
@@ -2,6 +2,18 @@ module Solutions.Day03a
   ( solve
   ) where
 
+import Debug.Trace
 
 solve :: String -> Int
-solve input = 0
+solve input = trace ("parsed input: " ++ show (parseInput input)) 0
+
+-- parsing
+
+parseInput :: String -> [[Int]]
+parseInput input = map parseLine $ lines input
+
+parseLine :: String -> [Int]
+parseLine = map parseCharacter
+
+parseCharacter :: Char -> Int
+parseCharacter c = read [c]
