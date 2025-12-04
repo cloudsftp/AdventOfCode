196db90010b8a9a0e981d451bd34576c7b8d30e6
Fabian Weik <fabian.weik@protonmail.com>
2025-12-04 07:59:39 +0100

    sketch structure of new approach

--- 2025h/src/Solutions/Day03b.hs
+++ 2025h/src/Solutions/Day03b.hs
@@ -4,8 +4,8 @@ module Solutions.Day03b
 
 import Debug.Trace
 
-import Data.Map (Map, insert, empty)
 import Data.Ix
+import Data.Map (Map, insert, empty, foldrWithKey, (!))
 
 type Bank = [Int]
 
@@ -16,30 +16,55 @@ solve input =
 
 bankJoltage :: Bank -> Int
 bankJoltage bank =
-  let digitOccurrences = digits bank
-  in trace ("occurrences: " ++ show digitOccurrences)
+  let digitOccurrences = digitPositions bank
+      selectedDigits = selectNDigits 9 9 (0, length bank) digitOccurrences
+  in trace ("occurrences: " ++ show digitOccurrences ++ " selected digits: " ++ show selectedDigits)
            0
+
+data Digit = Digit { digit :: Int
+                   , position :: Int
+                   } deriving (Show, Eq, Ord)
+
+selectNDigits :: Int -> Int -> (Int, Int) -> Map Int [Int] -> [Digit]
+selectNDigits _ 0 _ _ = []
+selectNDigits d n (l, r) digits
+  | r - l <= n =
+    let digitIsInRange Digit { position = p } = p >= l && p < r
+    in  filter digitIsInRange $ allDigits digits
+  | otherwise =
+    let result = [] -- choose as much digits d as possible
+    in result       -- if n still greater, recurse from right to left on ranges of left-over digits
+
+allDigits :: Map Int [Int] -> [Digit]
+allDigits = foldrWithKey collectDigits []
+
+collectDigits :: Int -> [Int] -> [Digit] -> [Digit]
+collectDigits d positions ds = foldl (\acc p -> toDigit d p:acc) ds positions
+
+toDigit :: Int -> Int -> Digit
+toDigit d p = Digit { digit = d, position = p }
 
 -- digit positions
 
-digits :: Bank -> Map Int [Int]
-digits bank = foldl (\digitOccurrences d -> insert d (occurrences d bank) digitOccurrences)
-                    empty $ range (1, 9)
+digitPositions :: Bank -> Map Int [Int]
+digitPositions bank =
+  let collectOccurrences d = insert d (occurrences d bank)
+  in foldl (flip collectOccurrences) empty $ range (1, 9)
 
-data DigitCounter = DigitCollecter { position :: Int
+data DigitCounter = DigitCollecter { current :: Int
                                    , collected :: [Int]
                                    } deriving (Show)
 
 occurrences :: Int -> Bank -> [Int]
 occurrences v bank = collected $
-  foldl (\DigitCollecter { position = pos, collected = coll } d ->
+  foldl (\DigitCollecter { current = pos, collected = coll } d ->
            let newPosition = pos + 1
                newCollected = if v == d
                               then pos:coll
                               else coll
-           in DigitCollecter { position = newPosition, collected = newCollected }
+           in DigitCollecter { current = newPosition, collected = newCollected }
         )
-        (DigitCollecter { position = 0, collected = [] })
+        (DigitCollecter { current = 0, collected = [] })
         bank
 
 -- parsing
