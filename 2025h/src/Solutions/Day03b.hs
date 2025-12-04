6108b4ff4e0d85cde71f575641d59cea293441b5
Fabian Weik <fabian.weik@protonmail.com>
2025-12-04 22:02:14 +0100

    day 3 part 2

--- 2025h/src/Solutions/Day03b.hs
+++ 2025h/src/Solutions/Day03b.hs
@@ -2,10 +2,7 @@ module Solutions.Day03b
   ( solve
   ) where
 
-import Debug.Trace
-
-import Data.Ix
-import Data.Map (Map, insert, empty, foldrWithKey, (!))
+import Data.List (elemIndex)
 
 type Bank = [Int]
 
@@ -15,99 +12,28 @@ solve input =
   in sum $ map bankJoltage banks
 
 bankJoltage :: Bank -> Int
-bankJoltage bank =
-  let digitOccurrences = digitPositions bank
-      largestDigit = 2
-      n = 3
-      selectedDigits =
-        selectNDigits largestDigit
-                      n
-                      (0, length bank)
-                      digitOccurrences
-  in trace ("selected digits: " ++ show selectedDigits ++ "\n")
-           0 -- compute number from selected digits
+bankJoltage bank = chooseNDigits 12 bank 0
 
-data Digit = Digit { digit :: Int
-                   , position :: Int
-                   } deriving (Show, Eq, Ord)
+chooseNDigits :: Int -> Bank -> Int -> Int
+chooseNDigits 0 _ sum = sum
+chooseNDigits n bank sum =
+  let position = cutPosition n bank
+      newBank = drop (position + 1) bank
+      newSum = 10 * sum + bank !! position
+  in chooseNDigits (n - 1) newBank newSum
 
-selectNDigits :: Int -> Int -> (Int, Int) -> Map Int [Int] -> [Digit]
-selectNDigits 0 _ _ _ = []
-selectNDigits _ n _ _ | n <= 0 = []
-selectNDigits d n (l, r) positions
-  | r - l <= n = filter digitIsInRange $ allDigits positions
-  | otherwise =
-    let digits = filter digitIsInRange $ matchingDigits d positions
-        numDigits = length digits
-    in
-      if numDigits >= n
-      then take n digits
-      else selectNDigitsRecursion d (n - length digits) (l, r) positions digits
-  where digitIsInRange Digit { position = p } = p >= l && p < r
-
-data DigitCollector = DigitCollector { digits :: [Digit]
-                                     , lastPosition :: Int
-                                     , nLeft :: Int
-                                     } deriving (Show)
-
-selectNDigitsRecursion :: Int -> Int -> (Int, Int) -> Map Int [Int] -> [Digit] -> [Digit]
-selectNDigitsRecursion d n (l, r) positions ds =
-  let collect digit DigitCollector { digits = ds
-                                   , lastPosition = right
-                                   , nLeft = nLeft
-                                   } = let currentLeft = position digit + 1
-                                           recDigits = selectNDigits (d - 1) nLeft (currentLeft, right) positions
-                                       in trace ("collecting for d: " ++ show d ++ ", digits: " ++ show ds ++ ", lastPosition: " ++ show right ++ ", nLeft: " ++ show nLeft)
-                                          DigitCollector { digits = recDigits ++ digit:ds
-                                                         , lastPosition = currentLeft
-                                                         , nLeft = nLeft - length recDigits
-                                                         }
-      DigitCollector { digits = collectedDigits
-                     , lastPosition = firstDigitPosition
-                     , nLeft = nLeftCollected
-                     } = foldr collect (DigitCollector { digits = []
-                                                       , lastPosition = r
-                                                       , nLeft = n
-                                                       }) ds
-  in trace ("collected digits: " ++ show collectedDigits ++ ", also have n left: " ++ show nLeftCollected)
-     selectNDigits nLeftCollected (d - 1) (l, firstDigitPosition) positions
-     ++ collectedDigits
-
-matchingDigits :: Int -> Map Int [Int] -> [Digit]
-matchingDigits d positions = map (toDigit d) $ positions ! d
-
-allDigits :: Map Int [Int] -> [Digit]
-allDigits = foldrWithKey collectDigits []
-
-collectDigits :: Int -> [Int] -> [Digit] -> [Digit]
-collectDigits d positions ds = foldl (\acc p -> toDigit d p:acc)
-                                     ds positions
-
-toDigit :: Int -> Int -> Digit
-toDigit d p = Digit { digit = d, position = p }
-
--- digit positions
-
-digitPositions :: Bank -> Map Int [Int]
-digitPositions bank =
-  let collectOccurrences d = insert d (occurrences d bank)
-  in foldl (flip collectOccurrences) empty $ range (1, 9)
-
-data PositionCollector = PositionCollector { current :: Int
-                                      , collected :: [Int]
-                                      } deriving (Show)
-
-occurrences :: Int -> Bank -> [Int]
-occurrences v bank = collected $
-  foldl (\PositionCollector { current = pos, collected = coll } d ->
-           let newPosition = pos + 1
-               newCollected = if v == d
-                              then pos:coll
-                              else coll
-           in PositionCollector { current = newPosition, collected = newCollected }
-        )
-        (PositionCollector { current = 0, collected = [] })
-        bank
+cutPosition :: Int -> Bank -> Int
+cutPosition n bank =
+  let len = length bank
+      recurse d
+        | d <= 0 = error "should never happen"
+        | otherwise = case elemIndex d bank of
+            Nothing -> recurse (d - 1)
+            Just position ->
+              if len - position < n
+              then recurse (d - 1)
+              else position
+  in recurse 9
 
 -- parsing
 
