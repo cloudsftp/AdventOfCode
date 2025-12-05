6a153512cc0e78ea71912fcdba2bddb89e09e4a5
Fabian Weik <fabian.weik@protonmail.com>
2025-12-05 07:35:37 +0100

    add split

--- 2025h/src/Solutions/Day05a.hs
+++ 2025h/src/Solutions/Day05a.hs
@@ -2,5 +2,12 @@ module Solutions.Day05a
   ( solve
   ) where
 
+import Data.List.Split (splitWhen)
+
 solve :: String -> Int
 solve input = 0
+
+-- parsing
+
+parseInput :: String -> ()
+parseInput _ = ()
