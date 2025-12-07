be299408129b9909ffacb3a032ab477d05b25f53
Fabian Weik <fabian.weik@protonmail.com>
2025-12-07 10:52:56 +0100

    parse day 7

--- 2025h/src/Solutions/Day07a.hs
+++ 2025h/src/Solutions/Day07a.hs
@@ -6,11 +6,29 @@ import Debug.Trace
 
 solve :: String -> Int
 solve input =
-  let _ = parseInput input
-  in trace ("parsed: ")
+  let (startPosition, splitterPositions) = parseInput input
+  in trace ("start position: " ++ show startPosition ++ " splitter positions: " ++ show splitterPositions)
      0
 
 -- parsing
 
-parseInput :: String -> ()
-parseInput _ = ()
+parseInput :: String -> (Int, [[Int]])
+parseInput input =
+  let firstLine:splitterLines = lines input
+      startPosition = parseStart firstLine
+      splitterPositions = map parseSplitterLine splitterLines
+      
+  in (startPosition, splitterPositions)
+
+parseStart :: String -> Int
+parseStart line =
+  let positions = filter ((=='S') . snd) $ enumerate line
+  in fst $ head positions
+
+parseSplitterLine :: String -> [Int]
+parseSplitterLine line =
+  map fst $ filter ((=='^') . snd) $ enumerate line
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = snd . foldl (\(pos, acc) e -> (pos + 1, (pos, e):acc)) (0, [])
+  
