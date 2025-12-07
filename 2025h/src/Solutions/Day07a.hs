90628c41538938086d406db85ec26c986178361c
Fabian Weik <fabian.weik@protonmail.com>
2025-12-07 11:05:17 +0100

    solve part 1 of day 7

--- 2025h/src/Solutions/Day07a.hs
+++ 2025h/src/Solutions/Day07a.hs
@@ -2,13 +2,22 @@ module Solutions.Day07a
   ( solve
   ) where
 
+import Data.Set (Set, singleton, member, insert, delete)
 import Debug.Trace
 
 solve :: String -> Int
 solve input =
   let (startPosition, splitterPositions) = parseInput input
   in trace ("start position: " ++ show startPosition ++ " splitter positions: " ++ show splitterPositions)
-     0
+     fst $ foldl step (0, singleton startPosition) splitterPositions
+
+step :: (Int, Set Int) -> [Int] -> (Int, Set Int)
+step (numberOfSplits, beamPositions) splitterPositions =
+  foldl (\(n, ps) p ->
+           if member p ps
+           then (n + 1, insert (p - 1) $ insert (p + 1) $ delete p  ps)
+           else (n, ps))
+  (numberOfSplits, beamPositions) splitterPositions
 
 -- parsing
 
