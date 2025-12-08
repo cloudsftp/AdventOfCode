df2ce42a63a75e9382bcfb5d91420ccaf0eb9d9b
Fabian Weik <fabian.weik@protonmail.com>
2025-12-08 20:29:57 +0100

    compute distances

--- 2025h/src/Solutions/Day08a.hs
+++ 2025h/src/Solutions/Day08a.hs
@@ -4,14 +4,41 @@ module Solutions.Day08a
 
 import Debug.Trace
 import Data.List.Split (splitWhen)
+import Data.List (sortOn)
+-- import Data.Heap (MinHeap, HeapItem, empty, insert)
 
-data Node = Node Int Int Int deriving (Show)
+data Node = Node Float Float Float deriving (Show)
+
+distance :: Node -> Node -> Float
+distance (Node xa ya za) (Node xb yb zb) =
+  sqrt $ (xa - xb) ^ 2 + (ya - yb) ^ 2 + (za - zb) ^ 2
+
+-- 
 
 solve :: String -> Int
 solve input =
   let nodes = parseInput input
-  in trace ("nodes: " ++ show nodes)
+      distances = computeDistances nodes
+  in trace ("nodes: " ++ show nodes ++ "\ndistances: " ++ show distances)
      0
+
+computeDistances :: [Node] -> [(Int, Int)]
+computeDistances nodes =
+  let indexPairs = [(i, j)
+                   | i <- [0..(length nodes - 1)]
+                   , j <- [(i+1)..(length nodes - 1)]]
+  in map snd
+        $ sortOn fst
+        $ collectPairDistances nodes indexPairs
+
+collectPairDistances :: [Node] -> [(Int, Int)] -> [(Float, (Int, Int))]
+collectPairDistances _ [] = []
+collectPairDistances nodes ((i, j):rest) =
+  let dist = distance (nodes !! i) (nodes !! j)
+  in (dist, (i, j)):collectPairDistances nodes rest
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = snd . foldl (\(pos, acc) e -> (pos + 1, (pos, e):acc)) (0, [])
 
 -- parsing
 
