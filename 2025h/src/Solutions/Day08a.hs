c3a2e62c0e03f56a267d7cdc68568fdf992aec9e
Fabian Weik <fabian.weik@protonmail.com>
2025-12-08 19:52:37 +0100

    parse nodes

--- 2025h/src/Solutions/Day08a.hs
+++ 2025h/src/Solutions/Day08a.hs
@@ -3,11 +3,22 @@ module Solutions.Day08a
   ) where
 
 import Debug.Trace
+import Data.List.Split (splitWhen)
+
+data Node = Node Int Int Int deriving (Show)
 
 solve :: String -> Int
-solve input = 0
+solve input =
+  let nodes = parseInput input
+  in trace ("nodes: " ++ show nodes)
+     0
 
 -- parsing
 
-parseInput :: String -> ()
-parseInput input = ()
+parseInput :: String -> [Node]
+parseInput = map parseLine . lines 
+
+parseLine :: String -> Node
+parseLine line =
+  let [x, y, z] = splitWhen (==',') line
+  in Node (read x) (read y) (read z)
