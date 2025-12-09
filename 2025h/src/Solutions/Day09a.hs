bca0559900db16e30b3bfc0ce87346fc01277835
Fabian Weik <fabian.weik@protonmail.com>
2025-12-09 07:26:34 +0100

    first attempt of part 1

--- 2025h/src/Solutions/Day09a.hs
+++ 2025h/src/Solutions/Day09a.hs
@@ -2,15 +2,40 @@ module Solutions.Day09a
   ( solve
   ) where
 
+import Data.List.Split (splitWhen)
+import Debug.Trace
+import Data.List (sortBy, sortOn)
+
 type Tile = (Int, Int)
 
 solve :: String -> Int
-solve input = 0
+solve input =
+  let tiles = parseInput input
 
--- areas 
+      topLeft = head $ sortOn (\(x, y) -> (x, y)) tiles
+      topRight = head $ sortOn (\(x, y) -> (-x, y)) tiles
+      bottomLeft = head $ sortOn (\(x, y) -> (x, -y)) tiles
+      bottomRight = head $ sortOn (\(x, y) -> (-x, -y)) tiles
+        
+  in trace ("tiles: " ++ show tiles
+            ++ " top left: " ++ show topLeft
+            ++ " top right: " ++ show topRight
+            ++ " bottom left: " ++ show bottomLeft
+            ++ " bottom right: " ++ show bottomRight)
+     max (area topLeft bottomRight) (area topRight bottomLeft)
+
+-- areas
+
+area :: Tile -> Tile -> Int
+area (xi, yi) (xj, yj) = (abs (xi - xj) + 1) * (abs (yi - yj) + 1)
 
 -- parsing
 
-parseInput :: String -> ()
-parseInput _ = ()
+parseInput :: String -> [Tile]
+parseInput = map parseTile . lines
+
+parseTile :: String -> Tile
+parseTile line =
+  let [x, y] = splitWhen (==',') line
+  in (read x, read y)
 
