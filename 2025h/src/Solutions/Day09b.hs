190e1f614331771a49003cf86d441420858d7514
Fabian Weik <fabian.weik@protonmail.com>
2025-12-09 13:51:49 +0100

    sort border tiles into two sets

--- 2025h/src/Solutions/Day09b.hs
+++ 2025h/src/Solutions/Day09b.hs
@@ -3,8 +3,9 @@ module Solutions.Day09b
   ) where
 
 import Data.List.Split (splitWhen)
+import Data.List (intersperse)
 import Debug.Trace
-import Data.Set (Set, empty)
+import Data.Set (Set, empty, insert, member)
 
 type Tile = (Int, Int)
 
@@ -16,13 +17,17 @@ solve input =
                     | i <- [0..(length corners - 1)]
                     , j <- [i..(length corners - 1)]]
 
-      borderTiles = collectBorders corners
+      (horizontalBorderTiles, verticalBorderTiles) = collectBorders corners
 
         
-  in trace ("corner pairs: " ++ show cornerPairs)
+  in trace ("corners: " ++ show corners
+            ++ "\ncorner pairs: " ++ show cornerPairs
+            ++ "\n\nhorizontal: " ++ show horizontalBorderTiles
+            ++ "\nvertical: " ++ show verticalBorderTiles
+            ++ "\n\n" ++ renderField corners (horizontalBorderTiles, verticalBorderTiles) ++ "\n")
      0
 
-collectBorders :: [Tile] -> Set Tile
+collectBorders :: [Tile] -> (Set Tile, Set Tile)
 collectBorders corners =
   let cornerPairs = [(corners !! i, corners !! j)
                     | i <- [0..(length corners - 1)]
@@ -32,12 +37,23 @@ collectBorders corners =
                           && j == 0)
                     ]
 
-      collect borderTiles ((xi, yi), (xj, yj)) = union borderTiles
+      collect (horizontalBorderTiles, verticalBorderTiles) ((xi, yi), (xj, yj)) = 
         if xi == xj
-        then foldl (\acc y)
-        else
+        then let (ySmall, yBig) = orderTuple (yi, yj)
+             in ( horizontalBorderTiles
+                , foldr insert verticalBorderTiles [(xi, y) | y <- [ySmall..yBig]]
+                )
+        else let (xSmall, xBig) = orderTuple (xi, xj)
+             in ( foldr insert horizontalBorderTiles [(x, yi) | x <- [xSmall..xBig]]
+                , verticalBorderTiles
+                )
       
-   in foldl collect empty cornerPairs
+   in foldl collect (empty, empty) cornerPairs
+
+orderTuple :: (Ord a) => (a, a) -> (a, a)
+orderTuple (i, j) = if i < j
+                    then (i, j)
+                    else (j, i)
 
 -- areas
 
@@ -56,3 +72,21 @@ parseTile line =
 
 -- visualizing
 
+renderField :: [Tile] -> (Set Tile, Set Tile) -> String
+renderField corners borderTiles =
+  let width = maximum (map fst corners) + 1
+      height = maximum (map snd corners) + 1
+  in concat
+  $ intersperse "\n"
+  $ map (renderLine corners borderTiles width) [0..height]
+
+renderLine :: [Tile] -> (Set Tile, Set Tile) -> Int -> Int -> String
+renderLine corners borderTiles width j =
+  map (renderTile corners borderTiles j) [0..width]
+
+renderTile :: [Tile] -> (Set Tile, Set Tile) -> Int -> Int -> Char
+renderTile corners (horizontalBorderTiles, verticalBorderTiles) j i =
+  if any (==(i, j)) corners then '+'
+  else if member (i, j) horizontalBorderTiles then '-'
+  else if member (i, j) verticalBorderTiles then '|'
+  else '.'
