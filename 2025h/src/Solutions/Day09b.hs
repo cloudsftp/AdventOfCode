c843a5a4cd63ab9e501058242f6b1def8aeb5d93
Fabian Weik <fabian.weik@protonmail.com>
2025-12-09 08:34:48 +0100

    WIP build up borders

--- 2025h/src/Solutions/Day09b.hs
+++ 2025h/src/Solutions/Day09b.hs
@@ -4,17 +4,40 @@ module Solutions.Day09b
 
 import Data.List.Split (splitWhen)
 import Debug.Trace
+import Data.Set (Set, empty)
 
 type Tile = (Int, Int)
 
 solve :: String -> Int
 solve input =
-  let tiles = parseInput input
+  let corners = parseInput input
 
-      indexPairs = [(i, j) | i <- [0..(length tiles - 1)], j <- [i..(length tiles - 1)]]
+      cornerPairs = [(corners !! i, corners !! j)
+                    | i <- [0..(length corners - 1)]
+                    , j <- [i..(length corners - 1)]]
+
+      borderTiles = collectBorders corners
+
         
-  in trace ("tiles: " ++ show tiles)
-     foldl max 0 [area (tiles !! i) (tiles !! j) | (i, j) <- indexPairs]
+  in trace ("corner pairs: " ++ show cornerPairs)
+     0
+
+collectBorders :: [Tile] -> Set Tile
+collectBorders corners =
+  let cornerPairs = [(corners !! i, corners !! j)
+                    | i <- [0..(length corners - 1)]
+                    , j <- [0..(length corners - 1)]
+                    , j == i + 1
+                      || (i == length corners - 1
+                          && j == 0)
+                    ]
+
+      collect borderTiles ((xi, yi), (xj, yj)) = union borderTiles
+        if xi == xj
+        then foldl (\acc y)
+        else
+      
+   in foldl collect empty cornerPairs
 
 -- areas
 
@@ -30,4 +53,6 @@ parseTile :: String -> Tile
 parseTile line =
   let [x, y] = splitWhen (==',') line
   in (read x, read y)
+
+-- visualizing
 
