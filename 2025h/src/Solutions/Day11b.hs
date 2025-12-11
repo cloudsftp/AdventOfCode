9713d4bc12257829402d594a07c84b6a1f011e7e
Fabian Weik <fabian.weik@protonmail.com>
2025-12-11 18:33:08 +0100

    attempt keeping track of visited set

--- 2025h/src/Solutions/Day11b.hs
+++ 2025h/src/Solutions/Day11b.hs
@@ -5,34 +5,38 @@ module Solutions.Day11b
 import Data.List (delete)
 import Data.List.Split (splitOn)
 import Debug.Trace
-import Data.Map (Map, empty, insert, (!))
+import Data.Map (Map, (!))
+import qualified Data.Map as Map (empty, insert)
+import Data.Set (Set, member)
+import qualified Data.Set as Set (empty, insert)
 
 solve :: String -> Int
 solve input =
   let connections = parseInput input
   in trace ("connections: " ++ show connections)
-     numPaths connections False False "you" -- "svr"
+     numPaths connections Set.empty "qlh"
 
-numPaths :: Map String [String] -> Bool -> Bool -> String -> Int
-numPaths _ visitedDAC visitedFFT "out" =
-  if visitedDAC && visitedFFT
-  then 1
-  else 0
-numPaths connections visitedDAC visitedFFT position =
-  let visitedDAC' = visitedDAC || position == "fft"
-      visitedFFT' = visitedFFT || position == "dac"
-      next = --trace ("accessing '" ++ position ++ "'")
-             connections ! position
-      
-  in sum $ map (numPaths connections visitedDAC' visitedFFT') next
+numPaths :: Map String [String] -> Set String -> String -> Int
+numPaths _ visited "out" =
+        if member "fft" visited && member "dac" visited
+        then 1
+        else 0
+numPaths connections visited position
+  | member position visited = 0
+  | otherwise =
+        let visited' = Set.insert position visited
+            next = trace ("accessing '" ++ position ++ "', num visited: " ++ show (length visited))
+                   connections ! position
+
+        in sum $ map (numPaths connections visited') next
 
 -- parsing
 
 parseInput :: String -> Map String [String]
 parseInput input =
   let connectionPairs = map parseLine $ lines input
-      collect (source, targets) = insert source targets
-  in foldr collect empty connectionPairs
+      collect (source, targets) = Map.insert source targets
+  in foldr collect Map.empty connectionPairs
 
 parseLine :: String -> (String, [String])
 parseLine line =
