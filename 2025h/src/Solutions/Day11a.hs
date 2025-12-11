26692e484bf7160e92183b71e43cdfe91d3cc63b
Fabian Weik <fabian.weik@protonmail.com>
2025-12-11 08:19:54 +0100

    solve part 1

--- 2025h/src/Solutions/Day11a.hs
+++ 2025h/src/Solutions/Day11a.hs
@@ -5,13 +5,19 @@ module Solutions.Day11a
 import Data.List (delete)
 import Data.List.Split (splitOn)
 import Debug.Trace
-import Data.Map (Map, empty, insert)
+import Data.Map (Map, empty, insert, (!))
 
 solve :: String -> Int
 solve input =
   let connections = parseInput input
   in trace ("connections: " ++ show connections)
-     0
+     numPaths connections "you"
+
+numPaths :: Map String [String] -> String -> Int
+numPaths _ "out" = 1
+numPaths connections position =
+  let next = connections ! position 
+  in sum $ map (numPaths connections) next
 
 -- parsing
 
